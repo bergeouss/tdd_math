a42a27f08bc2130e47c9c2589e1af22a66fcfb6c
bergeouss <bergeouss@gmail.com>
2020-04-14 03:14:04 +0200

    Added defs and test

---
+++ lib/3_or_5_multiples.rb
@@ -0,0 +1,9 @@
+
+def is_multiple_of_3_or_5?(n)
+  if n %3 == 0 || n %5 == 0
+    return true
+  end
+end
+  
+def sum_of_3_or_5_multiples(final_number)
+end
---
+++ spec/multiples_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/3_or_5_multiples.rb'
+
+describe "the 3 or 5 multiple" do
+  it  " " do
+    expect(3).to eq(true)
+  end
+end
+
+describe 'sum_of_3_and_5_multiples' do
+  it "sum_of_3_and_5_multiples" do
+    expect().to eq()
+  end
+end
